127864f382e276f0477adce3f82239674f41ddd1
GaryZhou98 <garyzhou98@gmail.com>
2025-03-07 19:22:32 -0800

    [Server] Create Table and Model for DB
    - Create users table which represent each user with login credenials
    - Create bookmarks table which represen each bookmark saved by a given user and metadata associated with the bookmark

---
+++ server/app/models.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from app import db
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+
+    articles = db.relationship("Article", back_populates="user")
+
+class Bookmark(db.Model):
+    __tablename__ = 'bookmarks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(2048), nullable=False)
+    title = db.Column(db.String(500), nullable=True)
+    favicon_url = db.Column(db.String(2048), nullable=True)
+    summary = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    user = db.relationship("User", back_populates="articles")
+
