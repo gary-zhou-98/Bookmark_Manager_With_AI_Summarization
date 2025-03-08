709b122172dc95a7f9eda102f4d53dc6fa5b8d13
GaryZhou98 <garyzhou98@gmail.com>
2025-03-07 19:07:58 -0800

    [Server] Initialize SQLAlchemy engine to connect to db
    - We have created a db called "bookmark_manager_dev" locally using pgadmin4
    - Install Flask-SQLAlchemy to use sqlAlchemy to connect and manage db connections

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,7 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+
+db = SQLAlchemy()
+
 
 def create_app():
+    # Load environment variables
+    load_dotenv()
+    
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    # Initialize DB with app
+    db.init_app(app)
 
     @app.route('/health')
     def health():
