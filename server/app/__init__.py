3425fe2d846d0863ee684f4bdc5f6c2fd8f3082d
GaryZhou98 <garyzhou98@gmail.com>
2025-03-08 16:23:05 -0800

    [Server] update __init__.py
    - Initialize Flask "app" instance at the top to be imported
    - Set correct db_url for app.config

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,28 +1,32 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import URL
 import os
 from dotenv import load_dotenv
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
 
+app = Flask(__name__)
 db = SQLAlchemy()
-jwt = JWTManager()
-bcrypt = Bcrypt()
+jwt = JWTManager(app)
+bcrypt = Bcrypt(app)
 
 def create_app():
     # Load environment variables
     load_dotenv()
-    
-    app = Flask(__name__)
-
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    db_url = URL.create(
+        "postgresql",
+        username=os.getenv("DATABASE_USER"),
+        password=os.getenv("DATABASE_PASSWORD"),  # Get password from environment
+        host=os.getenv("DATABASE_HOST"),
+        port=int(os.getenv("DATABASE_PORT")),  # port needs to be an integer
+        database=os.getenv("DATABASE_NAME")
+    )
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET")
 
-    # Initialize DB with app
     db.init_app(app)
-    jwt.init_app(app)
-    bcrypt.init_app(app)
 
     @app.route('/health')
     def health():
