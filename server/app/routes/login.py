9d02b2d09e5b06922ea01dcf27cc60e89cdf4327
GaryZhou98 <garyzhou98@gmail.com>
2025-03-09 22:20:56 -0700

    [Server] Configure CORS policy using flask-cors
    - Install flask-cors
    - Define CORS policy for endpoint /auth/login to allow request from localhost:3000

--- server/app/routes/login.py
+++ server/app/routes/login.py
@@ -1,28 +1,35 @@
-from app import db
 from app.models import User
 from app.service.authService import check_password
 from flask import request, jsonify, Blueprint
 from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_cors import CORS
 
 login_bp = Blueprint("login", __name__)
+CORS(login_bp, 
+     resources={r"/auth/login": {"origins": "http://localhost:3000"}},
+     supports_credentials=True,
+     methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+)
+
 
 @login_bp.post("/auth/login")
 def login():
   data = request.json
   email = data.get("email")
   password = data.get("password")
+  print(email, password)
 
   if not email or not password:
-    return jsonify({"message": "Email and password are required"}), 400
+    return jsonify({"error": "Email and password are required"}), 400
   
   user = User.query.filter_by(email=email).first()
   # Check if user exists
   if not user:
-    return jsonify({"message": "User does not exist"}), 400
+    return jsonify({"error": "User does not exist"}), 400
 
   # Check if password is correct
   if not check_password(user.password, password):
-    return jsonify({"message": "Invalid password"}), 401
+    return jsonify({"error": "Invalid password"}), 401
 
   # Create access token
   access_token = create_access_token(identity=user.id)
