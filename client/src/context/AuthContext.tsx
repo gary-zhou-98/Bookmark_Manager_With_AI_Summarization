37625cf8d0e9eb2cca89c883b90bdca9febfd1e6
GaryZhou98 <garyzhou98@gmail.com>
2025-03-09 21:29:10 -0700

    [Client] Create login request API call
    - Create new API folder with files for making api request
    - Create authAPI file
    - Create login api call to server uisng Axios
    - Invoke loginRequest in AuthContext login function

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,20 +1,36 @@
 "use client";
 
-import { createContext, useContext, useCallback } from "react";
+import { User } from "@/models/User";
+import { createContext, useState, useContext, useCallback } from "react";
+import { loginRequest } from "@/api/authAPI";
 
 interface AuthContextType {
   login: (email: string, password: string) => Promise<void>;
+  user: User | null;
+  accessToken: string | null;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+
   const login = useCallback(async (email: string, password: string) => {
-    console.log("logging in");
+    try {
+      await loginRequest(email, password).then((response) => {
+        setUser(response.user);
+        setAccessToken(response.accessToken);
+      });
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
   }, []);
 
   return (
-    <AuthContext.Provider value={{ login }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ login, user, accessToken }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
 
