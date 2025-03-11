a86179f657b958f31d13abfd60698b0eba6cae1d
GaryZhou98 <garyzhou98@gmail.com>
2025-03-10 20:49:18 -0700

    [Client] Hide Sign-up button if user is logged in
    - Per title

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -3,10 +3,11 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import "@/styles/header.css";
+import { useAuth } from "@/context/AuthContext";
 
 export default function Header() {
   const pathname = usePathname();
-
+  const { user, accessToken } = useAuth();
   return (
     <header className="header-container">
       <div className="header-content">
@@ -31,14 +32,16 @@ export default function Header() {
           >
             Login
           </Link>
-          <Link
-            href="/auth/register"
-            className={`nav-link ${
-              pathname === "/auth/register" ? "nav-link-active" : ""
-            }`}
-          >
-            Sign Up
-          </Link>
+          {!user && !accessToken && (
+            <Link
+              href="/auth/register"
+              className={`nav-link ${
+                pathname === "/auth/register" ? "nav-link-active" : ""
+              }`}
+            >
+              Sign Up
+            </Link>
+          )}
         </nav>
       </div>
     </header>
